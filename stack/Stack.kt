0594b5e2f07a5ce492a37dda8444a3a93edc5a88
richardgibson <rickityg@gmail.com>
2020-02-02 19:44:20 +0000

    move to github actions

--- stack/Stack.kt
+++ stack/Stack.kt
@@ -5,7 +5,8 @@ import io.kloudformation.unaryPlus
 
 class Stack: StackBuilder {
     override fun KloudFormation.create(args: List<String>) {
-        serverless("typescript-lambda-template", "prod", +"hexlabs-deployments") {
+        val environment = System.getenv("ENVIRONMENT") ?: "dev"
+        serverless("typescript-lambda-template", environment, +"hexlabs-deployments") {
             serverlessFunction("lambda", +args.first(), +"bundle.handler", +"nodejs12.x")
         }
     }
